1bfb048e72354ee47599de8a1a39b394b02f1539
yutoJ <yutoogata4@gmail.com>
2017-08-25 19:51:31 +0900

    add navbar and update devise

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def avatar_url(user)
+
+    if user.image
+      "http://graph.facebook.com/#{user.uid}/picture?type=large"
+    else
+      gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+      "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'pages#home'
+  get 'dashboard' => 'dashboards#index'
 
   devise_for :users,
               path: '',
